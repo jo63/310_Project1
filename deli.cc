e9d5f9f649a61c01b08489c292201f8c843449f0
Alvin Kang <dong.hyun.kang@duke.edu>
2015-02-04 18:07:13 -0500

    deli.cc: file input and threads as objects
    In progress version of deli.cc to handle input files and setting up
    objects (classes) for each thread.
    
    Need to fix main() to correctly take input argument of files.

--- deli.cc
+++ deli.cc
@@ -1,30 +1,86 @@
-#include <stdio.h> 	
-#include <unistd.h> 
-#include <assert.h> 
+#include <stdio.h>
+#include <unistd.h>
+#include <assert.h>
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+// Class for each Cashier thread
+class Cashier {
+
+	public:
+
+		vector<int> orderList;
+
+		int cur_order;
+
+		bool isEmpty(){
+			if (orderList.size() == 0) return true;
+			return false;
+		}
+
+		int getNextOrder(){
+			int temp = orderList.at(cur_order);
+			cur_order++;
+			return temp;
+		}
+
+};
 
 
 char* input_files;
 int cork_board_max, cashier_count;
 
+void readFile(vector<Cashier> cashiers){
+
+	vector<double> Main;
+    int count;
+    string lineData;
+    double tmp;
+
+    for(int i =0;i<i<sizeof(input_files);i++)
+    {
+    	//store this into a vector ifstream myfile (input_files[i], ios::in);
+    	vector<int> orders;
+    	ifstream infile;
+
+    	char* t = input_files[i];
+    	ifstream read(t);
+    	int temp;
+    	while(read>>temp)
+    	{
+    		orders.push_back(temp);
+    	}
+
+    	Cashier c = new Cashier(orders);
+    	cashiers.push_back(c);
+    }
+
+}
 
 
 void main (int argc, char *argv[]){
 	// Checks that there are enough inputs
 	if (argc <= 2) {
-		printf('More Inputs Required');
-		return; 
+		cout << 'More Inputs Required';
+		return;
 	}
-	
+
 	// Initializes Standard Global Variables
-	cork_board_max = argv[1]; 
+	cork_board_max = argv[1];
 	cashier_count = argc - 2;
 	input_files = (char*) argv[2];
-	
-	
-	
-	
-	
-	
-	
-	return; 
+
+
+	std::vector<Cashier> cashiers;
+	readFile(cashiers);
+
+
+
+
+
+	return;
+
+
 }
