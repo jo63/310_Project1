127726d1863dc03e117ffc9524ab73d317cf3882
Surnus15 <niklas.gahm@gmail.com>
2015-02-03 19:31:25 -0500

    Deli Invalid Input Test
    inv.in0 has a negative value which is impossible in the given boundary
    conditions, so we need to be able to notice and throw an error.
    
    May need to change the deli_invalid_test_true.txt depending on specific
    implementation.

--- deli_tester.sh
+++ deli_tester.sh
@@ -5,12 +5,19 @@ clear
 echo "Provided SW Test" 
 echo
 
-./deli 3 sw.in0 sw.in1 sw.in2 sw.in34 > deli_provided_test_res.txt
+./deli 3 sw.in0 sw.in1 sw.in2 sw.in3 sw.in4 > deli_provided_test_res.txt
 
-echo
 echo 
 
 ./diff deli_provided_test_true.txt deli_provided_test_res.txt
 
+echo
+echo 
+echo
 
+./deli 3 inv.in0 inv.in1 inv.in2 > deli_invalid_res.txt
+
+echo 
+
+./diff deli_invalid_test_true.txt deli_invalid_test_res.txt
 
