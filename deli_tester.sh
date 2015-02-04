aac992767cf22ea7be982bc03ca5695e2728580a
Surnus15 <niklas.gahm@gmail.com>
2015-02-03 19:48:22 -0500

    Performance Testing + Large Cork Boards
    Added performance timing test on all cases.
    
    Added a large board case.  Tests non-even emptying. (A cashier is
    emptied entirely early on.)

--- deli_tester.sh
+++ deli_tester.sh
@@ -5,7 +5,7 @@ clear
 echo "Provided SW Test" 
 echo
 
-./deli 3 sw.in0 sw.in1 sw.in2 sw.in3 sw.in4 > deli_provided_test_res.txt
+time ./deli 3 sw.in0 sw.in1 sw.in2 sw.in3 sw.in4 > deli_provided_test_res.txt
 
 echo 
 
@@ -13,9 +13,21 @@ echo
 
 echo
 echo 
+echo "Provided SW Test With Large Board" 
 echo
 
-./deli 3 inv.in0 inv.in1 inv.in2 > deli_invalid_res.txt
+time ./deli 5 sw.in0 sw.in1 sw.in2 sw.in3 sw.in4 > deli_provided_test_large_board_res.txt
+
+echo 
+
+./diff deli_provided_test_large_board_true.txt deli_provided_test_large_board_res.txt
+
+echo
+echo 
+echo "Invalid Sandwich Number Entry"
+echo 
+
+time ./deli 3 inv.in0 inv.in1 inv.in2 > deli_invalid_res.txt
 
 echo 
 
